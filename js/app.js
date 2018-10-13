ea83fa3ef269510a588b5801a23270a3d420fbed
ferllini13 <ferllini_13@hotmail.com>
2018-10-13 04:58:21 -0600

    web inicial

---
+++ js/app.js
@@ -0,0 +1,62 @@
+var app = angular.module("B2", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/login", {
+        templateUrl : "./../html/Login.html",
+        controller : 'loginCtrl'
+    })
+    .otherwise({
+        redirectTo: '/login'
+    });
+});
+
+
+app.controller('loginCtrl', function($scope,$location,connectApi){
+    $scope.user={};
+	$scope.starPage=function(){//condiciones iniciales
+        localStorage.clear();
+		localStorage= null;
+        document.getElementById('myForm').clear;                        
+    };
+
+    $scope.checkUser =  function(){//verificacion de la existencia de un usuario	
+        //metod de http angular
+        //connectApi.httpGet(method,msj).then(function(answer) {});
+        console.log($scope.user);
+        $location.url("main");
+
+    };
+})
+
+
+
+
+
+app.directive('menu', function() {
+    return {
+       templateUrl: 'html/menu.html',
+         controller:"menuCtrl"
+   };
+})
+
+/////////// hay que corregir la que el mae hace con las respueestas despues de jalarlas , pero este es el get y el post basico 
+
+//sevice que sobrecarga http con el fin de hacerlo accesible desde todos los comtroladores
+app.service('connectApi',function($http){
+	//implementacion del gttp.get
+	this.httpGet= function(method,requestJson){
+		var getPromise=$http.get(webSeviceIp + method+'?frase='+JSON.stringify(requestJson)).then(function (response){
+	    	return angular.fromJson(response.data.substring(73, response.data.length - 9));
+		});
+		return getPromise;
+	},
+	//implementacion del http.post
+	this.httpPost= function(method,requestJson){
+		var postPromise=$http.post(webSeviceIp+method, {frase:JSON.stringify(requestJson)}).then(function(response) {
+            console.log(response);
+	  		return angular.fromJson(response.data.d);
+       	});
+		return postPromise;
+	}
+})
