503d3fa2f9c893c7ee08327c0dfb95c983c67d1f
crisrivlop <crisrivlop@gmail.com>
2018-10-29 04:20:24 +0000

    get/post service

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 var app = angular.module("B2", ["ngRoute"]);
 
+const SERVER_IP = "http://localhost:3000/";
+
+
 app.config(function($routeProvider) {
     $routeProvider
     .when("/login", {
@@ -13,6 +16,18 @@ app.config(function($routeProvider) {
 
 
 app.controller('loginCtrl', function($scope,$location,connectApi){
+    connectApi.httpPost("login/signin",{username:"crisferlop1",password:"12345"})
+    .then(function(data){
+        console.log(data);    
+    });
+
+    connectApi.httpPost("login/1/login",{usuario:"crisferlop1",password:"12345"})
+    .then(function(data){
+        console.log(data);    
+    });
+    
+    console.log("login");
+
     $scope.user={};
 	$scope.starPage=function(){//condiciones iniciales
         localStorage.clear();
@@ -55,17 +70,24 @@ app.directive('menu', function() {
 //sevice que sobrecarga http con el fin de hacerlo accesible desde todos los comtroladores
 app.service('connectApi',function($http){
 	//implementacion del gttp.get
-	this.httpGet= function(method,requestJson){
-		var getPromise=$http.get(webSeviceIp + method+'?frase='+JSON.stringify(requestJson)).then(function (response){
-	    	return angular.fromJson(response.data.substring(73, response.data.length - 9));
+	this.httpGet= function(method){
+		var getPromise=$http.get(SERVER_IP+method).then(function (response){
+	    	return angular.fromJson(response);
+		});
+		return getPromise;
+    },
+    
+    this.httpGetR= function(method,requestJson){
+		var getPromise=$http.get(SERVER_IP+method, JSON.stringify(requestJson)).then(function (response){
+	    	return angular.fromJson(response);
 		});
 		return getPromise;
 	},
 	//implementacion del http.post
 	this.httpPost= function(method,requestJson){
-		var postPromise=$http.post(webSeviceIp+method, {frase:JSON.stringify(requestJson)}).then(function(response) {
+		var postPromise=$http.post(SERVER_IP+method, JSON.stringify(requestJson)).then(function(response) {
             console.log(response);
-	  		return angular.fromJson(response.data.d);
+	  		return angular.fromJson(response);
        	});
 		return postPromise;
 	}
